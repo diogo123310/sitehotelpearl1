b96247edbccda03082b4d335ca8445afa6537907
diogo123310 <diogoludobini@gmail.com>
2025-05-01 23:19:58 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+import './App.css';
+import HeaderComponent from './Components/headercomponent';
+
+function App() {
+  return (
+    <HeaderComponent/>
+  );
+}
+
+export default App;
---
+++ src/Components/headercomponent.js
@@ -0,0 +1,9 @@
+function HeaderComponent() {
+    return (
+        <div className="bg-blue-500 text-white p-4">
+            <img src="/Components/2025-05-01-22-42-18.png" alt="logo"></img>
+        </div>
+    );
+}
+
+export default HeaderComponent;
